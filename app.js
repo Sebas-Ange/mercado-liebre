ac21206f2fe3082d402e9e8ffcaf0a64bc1bbc4a
Sebas-Ange <angelicola.sebastian@gmail.com>
2022-03-27 13:01:06 -0300

    heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const publicPath = path.resolve(__dirname, "./public")
 
 app.use (express.static(publicPath))
 
-app.listen(3000,()=> console.log ("servidor funcionando"));
+app.listen(process.env.PORT || 3000,()=> console.log ("servidor funcionando"));
 
 
 app.get ("/",(req, res) => {
